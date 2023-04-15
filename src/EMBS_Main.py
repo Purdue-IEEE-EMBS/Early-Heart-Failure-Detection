0a8b4fc9bfcb705b3d171765cb70242d94fe16de
Atthin Chandrashekar <chand158@eceprog3.ecn.purdue.edu>
2023-04-15 13:32:03 -0400

    incomplete main

--- src/EMBS_Main.py
+++ src/EMBS_Main.py
@@ -1,9 +1,27 @@
 import calcAbsRate
 import calcPowerDensity
 import reflectionLoss
+import numpy as np
+import pandas as pd
 
 def main():
-    
 
+    lung_matrix = pd.read_csv("lung.csv").to_numpy()
+
+    tskin = 
+    rhoskin = 1
+    rel_permskin = 44.5
+
+    tfat = 
+    rhofat = 9.35
+    rel_permfat = 6.4
+
+    tmuscle = 
+    rhomuscle = 0.77
+    rel_permmuscle = 50.5
+
+    human_list_of_tups = [(tskin, rhoskin, rel_permskin), (tfat, rhofat, rel_permfat), (tmuscle, rhomuscle, rel_permmuscle), (-1, rholung, rel_permlung)]
+
+    for 
 if __name__ == "__main__":
     main()
